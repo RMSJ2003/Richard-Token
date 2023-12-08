d9ec914bb0fd4949032c4a6c291f0a06bd633be4
Richard <richardmontemayorsahagunjr@gmail.com>
2023-12-08 18:28:16 +0800

    Finished Authentication

--- src/token_frontend/src/index.jsx
+++ src/token_frontend/src/index.jsx
@@ -8,23 +8,28 @@ const init = async () => {
   const authClient = await AuthClient.create();
 
   // Checks if the user is already authenticated
+  // Works like a cookie
   if (await authClient.isAuthenticated()) {
-    console.log("logged in");
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      // Who is the identityProvider:
+      // The identityProvider is going to be a URL that points
+      // to the identity service on the Internet Computer, which
+      // basically provides the frontend for our login purposes so
+      // that we don't have to create it ourselves.
+      identityProvider: "https://identity.ic0.app/#authorize",
+      // What should happen once the log in was successful
+      onSuccess: () => {
+        // Once they have succesfully authenticated, is just to take our main app.
+        handleAuthenticated(authClient);
+      }
+    });
   }
+}
 
-  await authClient.login({
-    // Who is the identityProvider:
-    // The identityProvider is going to be a URL that points
-    // to the identity service on the Internet Computer, which
-    // basically provides the frontend for our login purposes so
-    // that we don't have to create it ourselves.
-    identityProvider: "https://identity.ic0.app/#authorize",
-    // What should happen once the log in was successful
-    onSuccess: () => {
-      // Once they have succesfully authenticated, is just to take our main app.
-      ReactDOM.render(<App />, document.getElementById("root"));
-    }
-  });
+async function handleAuthenticated(authClient) {
+  ReactDOM.render(<App />, document.getElementById("root"));
 }
 
 init();
